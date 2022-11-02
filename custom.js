c41bda079de08033ac76661145e0d3b43fbeb30d
Adriano Orlando <adrianoorlando04@gmail.com>
2022-11-03 11:58:22 +1300

    Add files via upload

--- custom.js
+++ custom.js
@@ -1,9 +1,3 @@
-$('body').on('mouseenter', '#ar-map-toggle', function(e) {
-  e.preventDefault();
-  $(".ar-map-container").addClass("ar-map-container--show");
-});
-
-$('body').on('mouseleave', '#ar-map-toggle', function(e) {
-  e.preventDefault();
-  $(".ar-map-container").removeClass("ar-map-container--show");
-});
+$( function() {
+    $( "#draggable" ).draggable();
+  } );
